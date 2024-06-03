a4bd398d7ff1be83665875d35e4075f3f276758f
Khalilul Afwan <148404261+khalilafwan@users.noreply.github.com>
2024-06-03 08:49:49 +0700

    test form

---
+++ forms.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Forms</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <form>
+        <div class="mb-3"><label for="exampleFormControlInput1">Email address</label><input class="form-control"
+                id="exampleFormControlInput1" type="email" placeholder="name@example.com"></div>
+        <div class="mb-3">
+            <label for="exampleFormControlSelect1">Example select</label><select class="form-control"
+                id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlSelect2">Example multiple select</label><select class="form-control"
+                id="exampleFormControlSelect2" multiple="">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+        <div class="mb-0"><label for="exampleFormControlTextarea1">Example textarea</label><textarea
+                class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea></div>
+    </form>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
