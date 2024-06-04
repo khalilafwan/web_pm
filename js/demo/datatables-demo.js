88127b0f379504ecfcb2d07243c16e9382e266ba
glody71 <glodysyah123@gmail.com>
2024-06-04 10:58:59 +0700

    data tables fixed kolom

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,10 +1,24 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    scrollX : true,
+  fixedColumns : true,
+  responsive: true,
+  lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+  pageLength: 102,
+  dom: "<'row'<'col-md-3'l><'col-md-7'f><'col-md-2 text-right'B>><'row'<'col-md-12't>><'row'<'col-md-6'i><'col-md-6'p>>",
+  autoWidth: false,    
+  language: {
+      search: "Search",
+      emptyTable: "No records found"
+     }
+  });
 });
 
 $.extend($.fn.dataTable.defaults, {
   scrollX : true,
+  fixedColumns : true,
+  responsive: true,
   lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
   pageLength: 52,
   dom: "<'row'<'col-md-3'l><'col-md-7'f><'col-md-2 text-right'B>><'row'<'col-md-12't>><'row'<'col-md-6'i><'col-md-6'p>>",
@@ -14,3 +28,4 @@ $.extend($.fn.dataTable.defaults, {
       emptyTable: "No records found"
      }
 });
+
