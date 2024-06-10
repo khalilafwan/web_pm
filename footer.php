6ea657160dee7a4b98433c957df8a61bc309f1ef
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-06-10 14:04:03 +0700

    footer

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
     <footer class="sticky-footer bg-white">
         <div class="container my-auto">
             <div class="copyright text-center my-auto">
-                <span>Copyright &copy; MBKM Universitas Negeri Padang</span>
+                <span>&copy; MBKM Universitas Negeri Padang 2024</span>
             </div>
         </div>
     </footer>
