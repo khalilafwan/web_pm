6c21e63cff122240a005a8af45c00dba81637e35
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-06-11 16:22:10 +0700

    ganti warna button

--- top-bar.php
+++ top-bar.php
@@ -83,7 +83,7 @@ require_once 'header.php';
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
+                    <a class="btn btn-danger" href="logout.php">Logout</a>
                 </div>
             </div>
         </div>
