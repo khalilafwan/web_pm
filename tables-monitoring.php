88344ec8b1d8a3ed126103636969ba870cfb4b91
Farraz Hadyan <farrazhadyan16@gmail.com>
2024-06-11 11:24:36 +0700

    y

--- tables-monitoring.php
+++ tables-monitoring.php
@@ -64,7 +64,7 @@ require_once 'header.php';
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataMonitoring" width="0%" cellspacing="0">
+                                <table class="table table-bordered" id="dataMonitoring" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th>ID PROJECT</th>
